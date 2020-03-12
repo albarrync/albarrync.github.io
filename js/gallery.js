c96bb3ea9109160aec9f9ea552b23ed8244c6dc0
Alex Barry <albarrync@gmail.com>
2020-03-11 21:48:01 -0400

    Fix js path

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,34 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+  let currentClass = "current";
+
+  thumbnails.forEach(function(thumbnail) {
+    // Preload large images.
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as display image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      let newImageAlt = thumbnail.alt;
+      mainImage.setAttribute("alt", newImageAlt);
+
+      // Change which image is current.
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Update clicked information in third column.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
